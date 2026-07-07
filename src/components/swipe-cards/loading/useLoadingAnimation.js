bea1de318167cdb0fddfdd711ade713401b172bd
Harsh <h4r5h.c0d3s@gmail.com>
2026-07-07 06:46:59 +0530

    feat(loader):setup the foundation of useLoadingAnimation

--- src/components/swipe-cards/loading/useLoadingAnimation.js
+++ src/components/swipe-cards/loading/useLoadingAnimation.js
@@ -1,16 +1,25 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useRef } from "react";
 
 gsap.registerPlugin(useGSAP);
 
 export function useLoadingAnimation(loaderRefs) {
-  const {
-    overlayRef,
-    barsRef,
-    progressRefs,
-  } = loaderRefs;
-
-  useGSAP(() => {
-
+  const timelines = useRef({
+    entrance: null,
+    exit: null,
   });
+
+  const equalizer = useRef({
+    running: false,
+    delayedCall: null,
+    previousScales: [],
+  });
+
+  useGSAP(
+    () => {
+      console.log("loader initialized");
+    },
+    { scope: loaderRefs.current.overlay },
+  );
 }
