2d5abfc225c8f793580c8f6c9d333079ddf84b09
Harsh <h4r5h.c0d3s@gmail.com>
2026-07-10 21:03:21 +0530

    feat(feed): trigger swipe loader on first viewport entry

--- src/pages/home/FeedPreview.jsx
+++ src/pages/home/FeedPreview.jsx
@@ -1,8 +1,28 @@
 import { SwipeCards } from "@/components/swipe-cards/SwipeCards";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef, useState } from "react";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export const FeedPreview = ({ mock_users }) => {
+  const feedSectionRef = useRef(null);
+  const [startLoader, setStartLoader] = useState(false);
+
+  useGSAP(() => {
+    ScrollTrigger.create({
+      trigger: feedSectionRef.current,
+      start: "top 45%",
+      once: true,
+      onEnter: () => setStartLoader(true),
+    });
+
+    ScrollTrigger.refresh();
+  }, []);
+
   return (
-    <section className="overflow-hidden">
+    <section ref={feedSectionRef} className="overflow-hidden">
       <h1 className="text-foreground text-4xl font-semibold capitalize text-center tracking-wide leading-11">
         see feed in action
       </h1>
@@ -10,7 +30,7 @@ export const FeedPreview = ({ mock_users }) => {
         swipe right for interested, swipe left to ignore
       </h3>
 
-      <SwipeCards users={mock_users} />
+      {startLoader && <SwipeCards users={mock_users} />}
     </section>
   );
 };
