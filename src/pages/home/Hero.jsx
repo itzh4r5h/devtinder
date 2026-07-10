ad704cf59afd6c875bc809406d07f169c697eeef
Harsh <h4r5h.c0d3s@gmail.com>
2026-07-10 20:39:07 +0530

    fix:heading overlflow outside section

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -16,7 +16,7 @@ export const Hero = ({user}) => {
         </div>
         {/* tag line ends */}
 
-        <h1 className="capitalize font-extrabold text-7xl tracking-wide leading-20">
+        <h1 className="capitalize font-extrabold text-6xl leading-15 sm:text-7xl tracking-wide sm:leading-20">
           find your <span className="text-bg">dev soulmate</span> <br /> 💻️{" "}
           <span className="inline-block translate-y-1.5">❤️</span>
         </h1>
