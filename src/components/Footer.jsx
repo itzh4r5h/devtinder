d8693be1e2be93e37964969cf11a1e7b88ea9ace
Harsh <h4r5h.c0d3s@gmail.com>
2026-07-11 11:35:39 +0530

    style: changed social links color and design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import { FaGithub, FaLinkedin, FaXTwitter } from "react-icons/fa6";
 import { Logo } from "./Logo";
+import { Button } from "./ui/button";
 
 export const Footer = () => {
-  const username = 'itzh4r5h'
+  const username = "itzh4r5h";
 
   return (
     <footer className="border-t border-white/10 container mx-auto px-5 flex flex-col items-center md:flex-row justify-between gap-6 py-8 mt-10">
@@ -15,32 +16,44 @@ export const Footer = () => {
       </p>
 
       {/* Socials */}
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-5">
         <a
           href={`https://github.com/${username}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="rounded-full border border-white/10 p-2 text-muted-foreground transition-all hover:border-primary/40 hover:text-primary"
         >
-          <FaGithub className="size-5" />
+          <Button
+            variant="outline"
+            className="button-bg cursor-pointer rounded-full size-10"
+          >
+            <FaGithub className="size-5" />
+          </Button>
         </a>
 
         <a
           href={`https://linkedin.com/in/${username}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="rounded-full border border-white/10 p-2 text-muted-foreground transition-all hover:border-primary/40 hover:text-primary"
         >
-          <FaLinkedin className="size-5" />
+          <Button
+            variant="outline"
+            className="button-bg cursor-pointer rounded-full size-10"
+          >
+            <FaLinkedin className="size-5" />
+          </Button>
         </a>
 
         <a
           href={`https://x.com/${username}`}
           target="_blank"
           rel="noopener noreferrer"
-          className="rounded-full border border-white/10 p-2 text-muted-foreground transition-all hover:border-primary/40 hover:text-primary"
         >
-          <FaXTwitter className="size-5" />
+          <Button
+            variant="outline"
+            className="button-bg cursor-pointer rounded-full size-10"
+          >
+            <FaXTwitter className="size-5" />
+          </Button>
         </a>
       </div>
     </footer>
